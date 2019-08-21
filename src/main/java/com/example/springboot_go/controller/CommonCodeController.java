0ced868034becbab7832e76dce3120748f877064
Duhoon <eclip6@naver.com>
2019-08-21 10:12:35 +0900

    ffffffff

--- src/main/java/com/example/springboot_go/controller/CommonCodeController.java
+++ src/main/java/com/example/springboot_go/controller/CommonCodeController.java
@@ -1,6 +1,8 @@
 package com.example.springboot_go.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -11,13 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping
 public class CommonCodeController {
     @RequestMapping(value = "/commonCode/edit")
     public String ActionMethod(){
         return "/commonCode/edit";
     }
+
+    @RequestMapping(value = "/commonCode/search")
+    public String ActionMethod2(){
+        return "/commonCode/list";
+    }
+
     @RequestMapping(value = "/commonCode/{action}", method = {RequestMethod.GET, RequestMethod.POST})
+    /* 이 부분은 맵으로 넘기는 것
     public ModelAndView edit(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelandView){
+
         String viewName = "/commonCode/";
         Map<String, Object> resultMap = new HashMap<String, Object>();
         if ("read".equals(action)){
@@ -28,4 +39,31 @@ public class CommonCodeController {
         modelandView.addObject("resultMap", resultMap);
         return modelandView;
     }
+    */
+    public ModelAndView edit(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelandView){
+        String viewName = "/commonCode/";
+        List<Object> resultList = new ArrayList<Object>();
+        if("list".equals(action)){
+            viewName = viewName + action;
+            Map<String, Object> data01 = new HashMap<String, Object>();
+            data01.put("NAME", "Don");
+            data01.put("COMMON_CODE_ID", 1);
+            data01.put("DESCRIPTION", "Handsome guy");
+            resultList.add(data01);
+            Map<String, Object> data02 = new HashMap<String, Object>();
+            data02.put("NAME", "Ann");
+            data02.put("COMMON_CODE_ID", 2);
+            data02.put("DESCRIPTION", "Pretty girl");
+            resultList.add(data02);
+            Map<String, Object> data03 = new HashMap<String, Object>();
+            data03.put("NAME", "Chen");
+            data03.put("COMMON_CODE_ID", 3);
+            data03.put("DESCRIPTION", "Extra guy");
+            resultList.add(data03);
+        }
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultList", resultList);
+        return modelandView;
+    }
+
 }
