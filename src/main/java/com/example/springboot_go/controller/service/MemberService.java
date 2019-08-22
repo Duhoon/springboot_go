85ef80c061af32281a579c83126e239e1a89bf69
Duhoon <eclip6@naver.com>
2019-08-22 09:40:54 +0900

    add

--- src/main/java/com/example/springboot_go/controller/service/MemberService.java
+++ src/main/java/com/example/springboot_go/controller/service/MemberService.java
@@ -1,16 +1,24 @@
 package com.example.springboot_go.controller.service;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.example.springboot_go.controller.dao.MemberDao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService{
+    /*
     public Object getObject(Object dataMap){
         Map<String, Object> resultObject = new HashMap<String, Object>();
         resultObject.put("MEMBER_ID", "293029301202");
         resultObject.put("NAME", "DUHOON");
+        return resultObject;
+    }
+    */
+    @Autowired
+    private MemberDao dao;
+    public Object getObject(Object dataMap){
+        Object resultObject = dao.getObject(dataMap);
         return resultObject;
     }
 }
